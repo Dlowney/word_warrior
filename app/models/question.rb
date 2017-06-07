a2e73f539c7e1dcb92eba871dc7b748b37c08df9
Douglas Lowney <lowney.douglas@gmail.com>
2017-06-07 09:24:42 +0200

    working gap-fill page

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,13 @@
 class Question < ApplicationRecord
 end
+
+
+# #calculate score(missing_word, user_input)
+# wrong_answer = []
+
+# if mw and ui == then its correct
+#   if not, then wrong answer << user input
+#     if wrong_answer != nil then results.wrong_answers = wrong_answer
+
+# #grabbing the wrong answers
+
