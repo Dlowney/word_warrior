01cc755f672b17fa80af031207df6b903f90fff2
Maxence Nesme <maxencenesme@gmail.com>
2017-06-13 17:17:58 +0200

    ..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
+    @attempts = Attempt.find(params[:id])
+    @answers = Answer.find(params[:id])
   end
 
   def new
