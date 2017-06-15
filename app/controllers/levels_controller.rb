45c50f302cc42303f6240978c0486b97fd4ff9ec
Douglas Lowney <lowney.douglas@gmail.com>
2017-06-15 15:48:59 +0200

    Profile & Level Pages: Performance Analytics

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,12 +1,13 @@
 class LevelsController < ApplicationController
   def index
     @levels = Level.all
+    @completed_head_words_group_by_level = HeadWord.completed_collection_group_by_level(current_user.id)
   end
 
   def show
     @level = Level.find(params[:id])
     @head_words = @level.head_words
-    @completed_head_words = @head_words.select { |hw| hw.completed?(current_user) }
-    @incomplete_head_words = @head_words - @completed_head_words
+    @completed_hw = HeadWord.completed_by_level(current_user.id, @level.id)
+    @incomplete_hw = @head_words - @completed_hw
   end
 end
--- app/models/head_word.rb
+++ app/models/head_word.rb
@@ -3,14 +3,30 @@ class HeadWord < ApplicationRecord
   has_many :questions
   has_many :answers, through: :questions
 
-  def completed?(user)
-    # Did the user correctly answer 10 questions belonging to this head word?
-    #
-    # are there 10 correct answers
-    # from the current_user
-    # belonging to any questions
-    # which belong to this head word
+  # @return Array<HeadWord>
+  def self.completed_collection(user_id)
+    HeadWord.find_by_sql("
+      SELECT head_words.*, answers.count
+      FROM head_words
+      INNER JOIN questions ON questions.head_word_id = head_words.id
+      INNER JOIN answers ON answers.question_id = questions.id
+      INNER JOIN attempts ON answers.attempt_id = attempts.id
+      WHERE answers.correct = true
+      AND attempts.user_id = #{user_id}
+      GROUP BY head_words.id
+      HAVING answers.count >= 10
+    ")
+  end
 
-    user.correct_answers_for_head_word(self.id).size >= 10
+  # @return Hash<Array<HeadWord>>
+  def self.completed_collection_group_by_level(user_id)
+    completed_collection = self.completed_collection(user_id)
+    completed_collection.group_by(&:level_id)
+  end
+
+  # @return Array<HeadWord>
+  def self.completed_by_level(user_id, level_id)
+    completed_collection = self.completed_collection(user_id)
+    completed_collection.select { |hw| hw.level_id == level_id }
   end
 end
